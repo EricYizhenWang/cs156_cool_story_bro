cc04496fa5ccfddc8900e29fb3e133fc40b829ee
Kenneth Hung <me@kenkhung.com>
2012-10-24 02:57:20 -0700

    Modulizing the constants and the I/O

---
+++ constants.py
@@ -0,0 +1,14 @@
+def trainSize():
+    return 94362233
+
+def hiddenSize():
+    return 1964391
+
+def validSize():
+    return 1965045
+
+def qualSize():
+    return 2749898
+
+def probeSize():
+    return 1374739
